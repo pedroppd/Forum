bc09e31f8dafc82c368278cc95dca1c273acac9e
pedroppd <pedro_pdantas@hotmail.com>
2022-02-12 21:39:24 -0300

    habilitando spring security

---
+++ src/main/java/br/com/forum/config/validation/security/SecurityConfigurations.java
@@ -0,0 +1,12 @@
+package br.com.forum.config.validation.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+
+}
