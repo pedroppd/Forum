ecb5c8eaeaa8ca30f761212f8a3fc6b956a03bae
pedroppd <pedro_pdantas@hotmail.com>
2022-02-19 09:32:00 -0300

    Adicionando token jwt no swagger

--- src/main/java/br/com/forum/config/validation/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/forum/config/validation/swagger/SwaggerConfiguration.java
@@ -3,10 +3,14 @@ package br.com.forum.config.validation.swagger;
 import br.com.forum.models.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -18,6 +22,15 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("br.com.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(User.class);
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(
+                Arrays.asList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para Token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()));
     }
 }
